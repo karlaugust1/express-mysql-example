2aebd8b3f7da22d2b2fd2feb8f24daaf6e88810a
Karl Harder <karl.harder@roit.com.br>
2021-04-26 23:15:38 -0300

    feat(my-sql): add initial connection structure for mysql

--- src/service/ContactService.ts
+++ src/service/ContactService.ts
@@ -1,6 +1,17 @@
+import { MySQL } from '../repository/test'
+
 export class ContactService {
 
     async save(contact: any){
+        const query = "select * from contacts"
+        MySQL.query(query, (err:Error, results:Object[])=>{
+            if(err){
+                console.log(err)
+            }
+            else {
+                console.log(results)
+            }
+        });
         Promise.resolve()
         console.log('YOLOO, btw', contact)
     }
